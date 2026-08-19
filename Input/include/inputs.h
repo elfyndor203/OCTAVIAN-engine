35527ce3f8839eb0e6f854706388bfaa52c44d6e
Elfyndor <176099230+elfyndor203@users.noreply.github.com>
2026-08-19 16:20:24 -0700

    removed inputsystem mouse reading

--- Input/include/inputs.h
+++ Input/include/inputs.h
@@ -8,9 +8,9 @@ typedef enum OCT_buttonStates {
     OCT_BUTTONSTATE_PRESSED,
     OCT_BUTTONSTATE_HELD
 } OCT_buttonStates;
-
-OCT_vec2 OCT_mouse_readPosWindow();
-OCT_vec2 OCT_mouse_readPosWorld();
+//
+// OCT_vec2 OCT_mouse_readPosWindow();
+// OCT_vec2 OCT_mouse_readPosWorld();
 float OCT_mouse_readScroll();
 
 OCT_buttonStates OCT_button_read(OCT_BUTTON button, double* timeSinceLastStateChange_out, OCT_index* framesSinceLastStateChange_out);
